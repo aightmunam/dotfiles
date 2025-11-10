6008a092a01f17bf6781205ee87724b9b1694abc
Munam Mubashir <38868658+aightmunam@users.noreply.github.com>
2025-11-10 02:04:33 +0100

    Improve basedpyright lsp functionality
    - Automatically finds poetry env and connects to it
    - For non-poetry env, uses the activated virtualenv
    - If not poetry and no activated virtualenv, falls back to global python

--- nvim/lsp/basedpyright.lua
+++ nvim/lsp/basedpyright.lua
@@ -1,47 +1,34 @@
 local function get_python_env(executable_name)
-  -- Return the path to the executable if $VIRTUAL_ENV is set and the binary exists somewhere beneath the $VIRTUAL_ENV path
   if vim.env.VIRTUAL_ENV then
-    local paths = vim.fn.glob(vim.env.VIRTUAL_ENV .. '/**/bin/' .. executable_name, true, true)
-    local executable_path = table.concat(paths, ', ')
-    if executable_path ~= '' then
-      vim.api.nvim_echo({ { 'Using path for ' .. executable_name .. ': ' .. executable_path, 'None' } }, false, {})
+    local executable_path = vim.env.VIRTUAL_ENV .. '/bin/' .. executable_name
+    -- Use filereadable to ensure the python executable actually exists
+    if vim.fn.filereadable(executable_path) == 1 then
+      vim.api.nvim_echo({ { 'Using $VIRTUAL_ENV path: ' .. executable_path, 'None' } }, false, {})
       return executable_path
     end
   end
 
-  -- find poetry.lock file in current directory or parent directories
   local poetry_lock = vim.fn.findfile('poetry.lock', '.;')
   if poetry_lock ~= '' then
-    -- use `poetry env info -p -C <path of folder containing poetry.lock>` to get the virtualenv path
-    local poetry_env_path = vim.fn.systemlist('poetry env info --path -C ' .. vim.fn.fnamemodify(poetry_lock, ':h'))
+    -- Use `poetry env info --path` to get the virtualenv path
+    -- -C changes the directory for the command
+    local poetry_env_path_list = vim.fn.systemlist('poetry env info --path -C ' .. vim.fn.fnamemodify(poetry_lock, ':h'))
 
-    if #poetry_env_path > 0 then
-      local candidate_paths = {}
-      for _, env_path in ipairs(poetry_env_path) do
-        if env_path ~= '' then
-          if string.sub(env_path, 0, 1) == '/' then
-            table.insert(candidate_paths, 0, env_path)
-          end
+    if #poetry_env_path_list > 0 then
+      local selected_poetry_env_path = poetry_env_path_list[1]
+      selected_poetry_env_path = vim.fn.trim((string.gsub(selected_poetry_env_path, '%s*%(Activated%)$', '')))
+      if selected_poetry_env_path ~= '' then
+        local venv_path_to_python = selected_poetry_env_path .. '/bin/' .. executable_name
+        if vim.fn.filereadable(venv_path_to_python) == 1 then
+          vim.api.nvim_echo({ { 'Using Poetry venv path: ' .. venv_path_to_python, 'None' } }, false, {})
+          return venv_path_to_python
         end
-      end
-      local selected_poetry_env_path = candidate_paths[0]
-
-      -- TODO: Evaluate if this might ever be needed
-      -- if #poetry_env_path > 1 then
-      --   for _, env_path in ipairs(candidate_paths) do
-      --     if string.find(selected_poetry_env_path, 'Activated') then
-      --       selected_poetry_env_path = string.gsub(selected_poetry_env_path, ' %(Activated%)', '')
-      --       selected_poetry_env_path = vim.fn.trim(selected_poetry_env_path)
-      --       break
-      --     end
-      --   end
-      -- end
-      local venv_path_to_python = selected_poetry_env_path .. '/bin/' .. executable_name
-      if vim.fn.filereadable(venv_path_to_python) == 1 then
-        return venv_path_to_python
       end
     end
   end
+
+  vim.api.nvim_echo({ { 'No venv found. Using global ' .. executable_name, 'WarnMsg' } }, false, {})
+  return nil
 end
 
 local active_python_path = get_python_env 'python'
