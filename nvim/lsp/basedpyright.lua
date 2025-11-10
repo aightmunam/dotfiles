9979ddae0208bf8be75916062d62c87f760cd969
Munam Mubashir <38868658+aightmunam@users.noreply.github.com>
2025-11-10 01:30:07 +0100

    Automatically find pythonPath for poetry projects
    - Find the poetry lock file, and run the poetry env info -p to activate
    the correct python virtualenv

--- nvim/lsp/basedpyright.lua
+++ nvim/lsp/basedpyright.lua
@@ -1,18 +1,50 @@
-local function set_python_path(command)
-  local path = command.args
-  local clients = vim.lsp.get_clients {
-    bufnr = vim.api.nvim_get_current_buf(),
-    name = 'basedpyright',
-  }
-  for _, client in ipairs(clients) do
-    if client.settings then
-      client.settings.python = vim.tbl_deep_extend('force', client.settings.python or {}, { pythonPath = path })
-    else
-      client.config.settings = vim.tbl_deep_extend('force', client.config.settings, { python = { pythonPath = path } })
+local function get_python_env(executable_name)
+  -- Return the path to the executable if $VIRTUAL_ENV is set and the binary exists somewhere beneath the $VIRTUAL_ENV path
+  if vim.env.VIRTUAL_ENV then
+    local paths = vim.fn.glob(vim.env.VIRTUAL_ENV .. '/**/bin/' .. executable_name, true, true)
+    local executable_path = table.concat(paths, ', ')
+    if executable_path ~= '' then
+      vim.api.nvim_echo({ { 'Using path for ' .. executable_name .. ': ' .. executable_path, 'None' } }, false, {})
+      return executable_path
     end
-    client:notify('workspace/didChangeConfiguration', { settings = nil })
+  end
+
+  -- find poetry.lock file in current directory or parent directories
+  local poetry_lock = vim.fn.findfile('poetry.lock', '.;')
+  if poetry_lock ~= '' then
+    -- use `poetry env info -p -C <path of folder containing poetry.lock>` to get the virtualenv path
+    local poetry_env_path = vim.fn.systemlist('poetry env info --path -C ' .. vim.fn.fnamemodify(poetry_lock, ':h'))
+
+    if #poetry_env_path > 0 then
+      local candidate_paths = {}
+      for _, env_path in ipairs(poetry_env_path) do
+        if env_path ~= '' then
+          if string.sub(env_path, 0, 1) == '/' then
+            table.insert(candidate_paths, 0, env_path)
+          end
+        end
+      end
+      local selected_poetry_env_path = candidate_paths[0]
+
+      -- TODO: Evaluate if this might ever be needed
+      -- if #poetry_env_path > 1 then
+      --   for _, env_path in ipairs(candidate_paths) do
+      --     if string.find(selected_poetry_env_path, 'Activated') then
+      --       selected_poetry_env_path = string.gsub(selected_poetry_env_path, ' %(Activated%)', '')
+      --       selected_poetry_env_path = vim.fn.trim(selected_poetry_env_path)
+      --       break
+      --     end
+      --   end
+      -- end
+      local venv_path_to_python = selected_poetry_env_path .. '/bin/' .. executable_name
+      if vim.fn.filereadable(venv_path_to_python) == 1 then
+        return venv_path_to_python
+      end
+    end
   end
 end
+
+local active_python_path = get_python_env 'python'
 
 return {
   cmd = { 'basedpyright-langserver', '--stdio' },
@@ -31,6 +63,7 @@ return {
   },
   settings = {
     python = {
+      pythonPath = active_python_path,
       analysis = {
         autoSearchPaths = true,
         typeCheckingMode = 'basic',
@@ -43,10 +76,24 @@ return {
   },
 
   on_attach = function(client, bufnr)
-    vim.api.nvim_buf_create_user_command(bufnr, 'LspPyrightSetPythonPath', set_python_path, {
-      desc = 'Reconfigure basedpyright with the provided python path',
-      nargs = 1,
-      complete = 'file',
+    local function update_python_path()
+      local new_path = get_python_env 'python'
+
+      -- Send a workspace/didChangeConfiguration notification
+      client.notify('workspace/didChangeConfiguration', {
+        settings = {
+          python = {
+            pythonPath = new_path,
+          },
+        },
+      })
+
+      vim.api.nvim_echo({ { 'LSP pythonPath updated to: ' .. (new_path or 'global'), 'None' } }, false, {})
+    end
+
+    vim.api.nvim_buf_create_user_command(bufnr, 'LspPyrightSetPythonPath', update_python_path, {
+      desc = 'Re-scan for Python venv and update basedpyright',
+      nargs = 0,
     })
   end,
 }
